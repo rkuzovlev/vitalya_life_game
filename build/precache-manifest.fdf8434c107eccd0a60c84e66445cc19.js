2ffed594980dfb1f2bd905de0eeab171a044fc9e
Kuzovlev Roman <rkuzovlev@gmail.com>
2019-10-13 12:45:46 +0700

    change homeroot

---
+++ build/precache-manifest.fdf8434c107eccd0a60c84e66445cc19.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e426df2a60dd7291b3d0ac2d7bf61f2",
+    "url": "/vitalya_life_game/index.html"
+  },
+  {
+    "revision": "770fa57d277a3d34edbb",
+    "url": "/vitalya_life_game/static/css/main.fc45762b.chunk.css"
+  },
+  {
+    "revision": "2ebf72109ad339096bad",
+    "url": "/vitalya_life_game/static/js/2.5884015b.chunk.js"
+  },
+  {
+    "revision": "770fa57d277a3d34edbb",
+    "url": "/vitalya_life_game/static/js/main.d7dfa7bf.chunk.js"
+  },
+  {
+    "revision": "43b2cf1c4ea22962e81a",
+    "url": "/vitalya_life_game/static/js/runtime-main.68e44c92.js"
+  },
+  {
+    "revision": "0c7e1b66ad923a1e9e5b29a09a432097",
+    "url": "/vitalya_life_game/static/media/banka.0c7e1b66.png"
+  },
+  {
+    "revision": "c16cfac04953b8d4cdde1c811ef8701f",
+    "url": "/vitalya_life_game/static/media/clock.c16cfac0.png"
+  },
+  {
+    "revision": "292f915643e39115be8428215f1c593d",
+    "url": "/vitalya_life_game/static/media/hero.292f9156.png"
+  },
+  {
+    "revision": "d72eba5648392027933e6ef2e855b4f5",
+    "url": "/vitalya_life_game/static/media/road.d72eba56.png"
+  },
+  {
+    "revision": "74e7b1f721147c943b9978311fad7e9c",
+    "url": "/vitalya_life_game/static/media/sound.74e7b1f7.mp3"
+  },
+  {
+    "revision": "a8a3ac914c262ec3b5de9b2fa82a4c6d",
+    "url": "/vitalya_life_game/static/media/teplica.a8a3ac91.png"
+  }
+]);
